e95f95b13cd677740c458d8c505fb21beedf2450
svant07 <antsvetlanka07@gmail.com>
2024-07-19 10:37:30 +0300

    commit 1

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while True:
+    if my_list[a] > 0:
+        print(my_list[a])
+        a = a + 1
+    elif my_list[a] == 0:
+        a = a + 1
+    elif my_list[a] < 0:
+        break
+    elif my_list[a] == my_list[-1]:
+        if a > 0:
+            print(my_list[a])
+            break
