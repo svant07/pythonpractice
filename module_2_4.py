4f8585659bd5efd00eee0f5b78b21b6a04282c92
svant07 <antsvetlanka07@gmail.com>
2024-07-23 19:32:57 +0300

    commit 3

--- module_2_4.py
+++ module_2_4.py
@@ -2,16 +2,17 @@ primes = []
 not_primes = []
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 is_prime = True
-for i in range(1,16):
+for i in numbers:
+    if i == 1:
+        continue
+    #print(i)
     for j in range(2, i):
         if i % j == 0:
             not_primes.append(i)
-            continue
-
-        else:
-            #i = is_prime
-            primes.append(i)
+            #is_prime = False
             break
+    if i not in not_primes:
+            primes.append(i)
+            #is_prime = True
 print('Primes: ', primes)
 print('Not primes: ', not_primes)
-
---
+++ module_2_5.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+
+for i in
+    result1 = get_matrix(2, 2, 10)
+    result2 = get_matrix(3, 5, 42)
+    result3 = get_matrix(4, 2, 13)
+    print(result1)
+    print(result2)
+    print(result3)
