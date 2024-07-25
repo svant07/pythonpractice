cc493b7ec6eb50f76d029be3fa8494ee34583cb9
svant07 <antsvetlanka07@gmail.com>
2024-07-25 17:51:01 +0300

    commit 3_2_1

--- module_3_2.py
+++ module_3_2.py
@@ -1,11 +1,9 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-
-    if "@" in recipient and ".com" in recipient or ".ru" in recipient or ".net" in recipient: # "@" in sender or ".com" in sender or ".ru" in sender or ".net" in sender:
-        if "@" in sender and ".com" in sender or ".ru" in sender or ".net" in sender:
+    if "@" in recipient and ".com" in recipient or ".ru" in recipient or ".net" in recipient:
 
             if sender == recipient:
                 print('Нельзя отправить письмо самому себе!')
-        # continue
+
             elif sender == 'university.help@gmail.com':
                 print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
             else:
@@ -21,4 +19,3 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
