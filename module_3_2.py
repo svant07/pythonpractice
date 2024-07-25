2e5d12767fc5f41b55e070f31ecfd3a6c4eddffe
svant07 <antsvetlanka07@gmail.com>
2024-07-25 17:49:24 +0300

    commit 3_2

---
+++ module_3_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    # for i in range():
+    if "@" in recipient and ".com" in recipient or ".ru" in recipient or ".net" in recipient: # "@" in sender or ".com" in sender or ".ru" in sender or ".net" in sender:
+        if "@" in sender and ".com" in sender or ".ru" in sender or ".net" in sender:
+
+            if sender == recipient:
+                print('Нельзя отправить письмо самому себе!')
+        # continue
+            elif sender == 'university.help@gmail.com':
+                print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+            else:
+                print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+        else:
+            print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+
+    else:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
