352b3008e43ba31512dd4bc34f1ab792f4c9b0fe
svant07 <antsvetlanka07@gmail.com>
2024-07-25 17:56:50 +0300

    commit 3_2_2

--- module_3_2.py
+++ module_3_2.py
@@ -1,16 +1,14 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if "@" in recipient and ".com" in recipient or ".ru" in recipient or ".net" in recipient:
-
+        if "@" in sender and ".com" in sender or ".ru" in sender or ".net" in sender:
             if sender == recipient:
                 print('Нельзя отправить письмо самому себе!')
-
             elif sender == 'university.help@gmail.com':
                 print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
             else:
                 print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
         else:
             print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
-
     else:
         print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
 
