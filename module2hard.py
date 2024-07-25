fe0f0c5ae43516a7aeef170f00b5855588e57129
svant07 <antsvetlanka07@gmail.com>
2024-07-25 13:01:02 +0300

    commit 5

---
+++ module2hard.py
@@ -0,0 +1,28 @@
+
+
+
+def get_random_num():
+    numbers = list(range(3, 21))
+    random_num = int(input('Введите число от 3 до 20: '))
+    return random_num
+
+
+n = get_random_num()
+
+first = list(range(1, n))
+second = list(range(1, n))
+passw = []
+result = ''
+for i in first:
+    for j in second:
+        if i >= j:
+            continue
+        else:
+            a = n % (i + j)
+            if a == 0:
+                passw.append(i)
+                passw.append(j)
+                result = result + str(i) + str(j)
+
+print(result)
+
--- module_2_5.py
+++ module_2_5.py
@@ -1,10 +1,16 @@
 def get_matrix(n, m, value):
     matrix = []
+    print(matrix)
+  
 
-for i in
-    result1 = get_matrix(2, 2, 10)
-    result2 = get_matrix(3, 5, 42)
-    result3 = get_matrix(4, 2, 13)
-    print(result1)
-    print(result2)
-    print(result3)
+for i in range(n):
+    for j in range(m):
+        matrix.append(value)
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
