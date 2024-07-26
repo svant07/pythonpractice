3ef6e0faec2a1b25e9b71ea11b7fea4f3a67e958
svant07 <antsvetlanka07@gmail.com>
2024-07-26 22:42:58 +0300

    commit 3_4

---
+++ module_3_4.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word = root_word.lower()
+    for i in range(len(other_words)):
+        if root_word in other_words[i].lower() or other_words[i].lower() in root_word:
+            same_words.append(other_words[i])
+        else:
+            continue
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
